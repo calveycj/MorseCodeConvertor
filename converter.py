d69c7ea3110891cc6e381bfd52acc27ac91fe832
Connor Calvey <connorcalvey@gmail.com>
2025-10-20 11:51:40 +0100

    Updated text_to_morse method to handle strings with a space in them

--- converter.py
+++ converter.py
@@ -3,5 +3,8 @@ from morse_code_dict import MORSE_CODE_DICT
 def text_to_morse(string):
     answer = ""
     for char in string.upper():
-        answer += MORSE_CODE_DICT[char]
+        if char == " ":
+            answer += ", "
+        elif char in MORSE_CODE_DICT:
+            answer += MORSE_CODE_DICT[char]
     return answer.strip()
